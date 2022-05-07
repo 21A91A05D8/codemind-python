e5b4d0e1fd86d8369e0d0a09c007ce03c1da6481
21A91A05D8 <97424615+21A91A05D8@users.noreply.github.com>
2022-05-07 10:33:23 +0530

    n=int(input()) a=list(map(int,input().split())) f=0 for i in range(0,n):     for j in range(i+1,n):         if a[i]==a[j]:             f=1             break     if(f==1):         break print(a[i])

---
+++ Find_the_Duplicate_Number.py
@@ -0,0 +1,11 @@
+n=int(input())
+a=list(map(int,input().split()))
+f=0
+for i in range(0,n):
+    for j in range(i+1,n):
+        if a[i]==a[j]:
+            f=1
+            break
+    if(f==1):
+        break
+print(a[i])

09d91748a113af581138e28465a0972cd4809968
21A91A05D8 <97424615+21A91A05D8@users.noreply.github.com>
2022-05-27 22:48:18 +0530

    a=int(input()) for k in range(a):     b=input()     dc=0     for i in b:         if i.isdigit():             dc+=1     if(dc!=0):         print("Yes")     else:         print("No")

---
+++ string_contains_digits_or_not.py
@@ -0,0 +1,12 @@
+a=int(input())
+for k in range(a):
+    b=input()
+    dc=0
+    for i in b:
+        if i.isdigit():
+            dc+=1
+    if(dc!=0):
+        print("Yes")
+    else:
+        print("No")
+            

862e17b391aa7e1d874b12c21e396d7eeb0528bc
21A91A05D8 <97424615+21A91A05D8@users.noreply.github.com>
2022-06-04 11:16:33 +0530

    s=input() zz=0 co=0 for i in s:     if i=='z':         zz+=1     elif i=='o':         co+=1 if(zz*2==co):     print("Yes") else:     print("No")

---
+++ Zoos.py
@@ -0,0 +1,25 @@
+s=input()
+zz=0
+co=0
+for i in s:
+    if i=='z':
+        zz+=1
+    elif i=='o':
+        co+=1
+if(zz*2==co):
+    print("Yes")
+else:
+    print("No")
+
+
+
+
+
+
+
+
+
+
+
+
+

d715cbf2ad66616eaabc68c56bb378a00cac635c
21A91A05D8 <97424615+21A91A05D8@users.noreply.github.com>
2022-06-12 14:57:02 +0530

    a=input() arr=list(a.split()) vow=list("aeiouAEIOU") ma=100
    for i in arr:
        c=0
        for j in i:
            if j in vow:
                c+=1
        if ma>c:
            ma=c
    
    print(ma)

---
+++ minimum_vowel_count_in_a_word.py
@@ -0,0 +1,14 @@
+a=input()
+arr=list(a.split())
+vow=list("aeiouAEIOU")
+ma=100
+
+for i in arr:
+    c=0
+    for j in i:
+        if j in vow:
+            c+=1
+    if ma>c:
+        ma=c
+
+print(ma)

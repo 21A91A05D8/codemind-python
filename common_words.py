06975ae3ff8e80eec3bfda82cde2ac7657b078a8
21A91A05D8 <97424615+21A91A05D8@users.noreply.github.com>
2022-07-16 19:16:34 +0530

    a=input() b=input() a=a.lower() b=b.lower() a=a.split() b=b.split() for i in range(0,len(b)):     if b[i] in a:         print(b[i],end=' ')

--- common_words.py
+++ common_words.py
@@ -1,26 +1,9 @@
 a=input()
 b=input()
-a1=""
-b1=""
-for i in a:
-    if i.isupper():
-        a1+=i.lower()
-    else:
-        a1+=i
-for i in b:
-    if i.isupper():
-        b1+=i.lower()
-    else:
-        b1+=i
-arr=list(a1.split())
-brr=list(b1.split())
-res=[]
-for i in brr:
-    if i in arr:
-        if i not in res:
-            res.append(i)
-if len(res)==0:
-    print(0)
-else:
-    for i in res:
-        print(i,end=" ")
+a=a.lower()
+b=b.lower()
+a=a.split()
+b=b.split()
+for i in range(0,len(b)):
+    if b[i] in a:
+        print(b[i],end=' ')

062fccd6c7f026ea800c5921938bf0d2b31b6676
21A91A05D8 <97424615+21A91A05D8@users.noreply.github.com>
2022-07-16 11:19:33 +0530

    a,b= map(int,input().split()) c = list(map(int,input().split())) d = list(map(int,input().split())) e = [] for i in c:     if i not in d:         if i not in e:             e.append(i) for j in d:     if j not in c:         if j not in e:             e.append(j)
    print(*e)

---
+++ Common_elements_II.py
@@ -0,0 +1,15 @@
+a,b= map(int,input().split())
+c = list(map(int,input().split()))
+d = list(map(int,input().split()))
+e = []
+for i in c:
+    if i not in d:
+        if i not in e:
+            e.append(i)
+for j in d:
+    if j not in c:
+        if j not in e:
+            e.append(j)
+
+            
+print(*e)

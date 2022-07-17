a8b27731c7af3ad98aa6f3d8c761f6f527aa4a31
21A91A05D8 <97424615+21A91A05D8@users.noreply.github.com>
2022-07-17 11:13:43 +0530

    s1 = input().lower() s2 = input().lower() s1 =s1.replace(" ","") s2 =s2.replace(" ","") a = '' for i in s1:     if i not in s2 and i not in a:         a+=i for i in s2:     if i not in s1 and i not in a:         a+=i a=sorted(a) print(len(a))

---
+++ Non_common_characters.py
@@ -0,0 +1,13 @@
+s1 = input().lower()
+s2 = input().lower()
+s1 =s1.replace(" ","")
+s2 =s2.replace(" ","")
+a = ''
+for i in s1:
+    if i not in s2 and i not in a:
+        a+=i
+for i in s2:
+    if i not in s1 and i not in a:
+        a+=i
+a=sorted(a)
+print(len(a))

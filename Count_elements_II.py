ec018ef763e36a8e77eb37e16a1189bb75b4296a
21A91A05D8 <97424615+21A91A05D8@users.noreply.github.com>
2022-07-17 21:34:06 +0530

    a,b= map(int,input().split()) c = list(map(int,input().split())) d = list(map(int,input().split())) e = [] for i in c:     if i not in d:         if i not in e:             e.append(i) for j in d:     if j not in c:         if j not in e:             e.append(j)
    print(len(e))

--- Count_elements_II.py
+++ Count_elements_II.py
@@ -1,13 +1,15 @@
-a,b=map(int,input().split())
-c=list(map(int,input().split()))
-d=list(map(int,input().split()))
-#c=set(c)
-#d=set(d)
-e=0
-for i in list(set(c)):
-    if d.count(i)==0:
-        e+=1
-for j in list(set(d)):
-    if c.count(j)==0:
-        e+=1
-print(e)
+a,b= map(int,input().split())
+c = list(map(int,input().split()))
+d = list(map(int,input().split()))
+e = []
+for i in c:
+    if i not in d:
+        if i not in e:
+            e.append(i)
+for j in d:
+    if j not in c:
+        if j not in e:
+            e.append(j)
+
+            
+print(len(e))

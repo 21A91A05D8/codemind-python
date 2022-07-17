1306ef1c0f9d9f5b0d123f5f51f222e41f989eb3
21A91A05D8 <97424615+21A91A05D8@users.noreply.github.com>
2022-07-17 11:46:39 +0530

    s1 = input().lower() s2 = input().lower() s1,s2=s1.split(" "),s2.split(" ") c = 0 for i in s1:     if s1.count(i)==1 and s2.count(i)==1:         c+=1 print(c)

--- common_words_-1.py
+++ common_words_-1.py
@@ -1,31 +1,8 @@
-def word(s):
-    s1=""
-    for i in s:
-        s1+=i.lower()
-    return s1
-a=input()
-b=input()
-c=list(a.split())
-d=list(b.split())
-w1=[]
-for i in c:
-    if word(i) not in w1:
-        w1.append(word(i))
-w2=[]
-for i in d:
-    if word(i) not in w2:
-        w2.append(word(i))
-co=0
-if len(w1)>len(w2):
-    for i in w2:
-        if i in w1:
-            co+=1
-elif len(w1)<len(w2):
-    for i in w1:
-        if i in w2:
-            co+=1
-else:
-    for i in w1:
-        if i in w2:
-            co+=1
-print(co)
+s1 = input().lower()
+s2 = input().lower()
+s1,s2=s1.split(" "),s2.split(" ")
+c = 0
+for i in s1:
+    if s1.count(i)==1 and s2.count(i)==1:
+        c+=1
+print(c)

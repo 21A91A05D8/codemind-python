95c781c8059fccd160491869c2842dd4e7ac6470
21A91A05D8 <97424615+21A91A05D8@users.noreply.github.com>
2022-07-17 21:32:01 +0530

    a,b= map(int,input().split()) c = list(map(int,input().split())) d = list(map(int,input().split())) e = [] for i in c:     if i in d:         if i not in e:             e.append(i) for j in d:     if j in c:         if j not in e:             e.append(j)
    print(len(e))

--- Count_elements_I.py
+++ Count_elements_I.py
@@ -1,7 +1,15 @@
-a,b=map(int,input().split())
-c=list(map(int,input().split()))
-d=list(map(int,input().split()))
-c=set(c)
-d=set(d)
-e=c.intersection(d)
+a,b= map(int,input().split())
+c = list(map(int,input().split()))
+d = list(map(int,input().split()))
+e = []
+for i in c:
+    if i in d:
+        if i not in e:
+            e.append(i)
+for j in d:
+    if j in c:
+        if j not in e:
+            e.append(j)
+
+            
 print(len(e))

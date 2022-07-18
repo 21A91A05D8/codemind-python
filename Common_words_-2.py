ae621998ac7f42549748e14cc046f4a3f5b2b40f
21A91A05D8 <97424615+21A91A05D8@users.noreply.github.com>
2022-07-18 10:09:03 +0530

    a=input().lower().split() b=input().lower().split() c=0 for i in a:     if i in b:         if(a.count(i)==1 and b.count(i)==1):             c+=1 print(c)

--- Common_words_-2.py
+++ Common_words_-2.py
@@ -2,6 +2,7 @@ a=input().lower().split()
 b=input().lower().split()
 c=0
 for i in a:
-    if a.count(i)==1 and b.count(i)==1:
-        c+=1
+    if i in b:
+        if(a.count(i)==1 and b.count(i)==1):
+            c+=1
 print(c)

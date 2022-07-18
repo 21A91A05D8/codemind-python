f8b27302f3110ff3b20522d8f29ee7721c737e00
21A91A05D8 <97424615+21A91A05D8@users.noreply.github.com>
2022-07-18 10:13:59 +0530

    s=input().lower().split() c="" for i in s[0]:     f=0     for j in s[1:]:         if i in j:             f=1         else:             f=0             break     if f==1:         c+=i if len(c)==0:     print(-1) else:     print(c)

--- Common_characters_-1.py
+++ Common_characters_-1.py
@@ -1,15 +1,16 @@
-s = input().lower()
-s = s.split(" ")
-a = s[0]
-x = ''
-for i in a:
-    count = 0
-    for j in range(1,len(s)):
-        if i in s[j]:
-            count+=1
-    if count==len(s)-1:
-        x+=i
-if len(x)==0:
-    print("-1")
+s=input().lower().split()
+c=""
+for i in s[0]:
+    f=0
+    for j in s[1:]:
+        if i in j:
+            f=1
+        else:
+            f=0
+            break
+    if f==1:
+        c+=i
+if len(c)==0:
+    print(-1)
 else:
-    print(x)
+    print(c)

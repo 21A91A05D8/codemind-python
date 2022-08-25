c64ec99b613ee0c90b6616aabcbc1172c7c88888
21A91A05D8 <97424615+21A91A05D8@users.noreply.github.com>
2022-08-25 18:36:33 +0530

    s=input().split(" ") for i in range(len(s)):     a=list(s[i])     for j in range(len(a)):         for k in range(j+1,len(a)):             if a[j]>a[k] and a[j] not in '`~!@#$%^&*()_-;:"<>,./?' and a[k] not in '`~!@#$%^&*()_-;:"<>,./?':                 a[j],a[k]=a[k],a[j]     a = str(a)     a = a.replace("[","")     a = a.replace("]","")     a = a.replace(" ","")     a = a.replace(",","")     a = a.replace("[","")     a = a.replace("'","")     print(a,end=" ")

---
+++ sort_words_of_a_string.py
@@ -0,0 +1,15 @@
+s=input().split(" ")
+for i in range(len(s)):
+    a=list(s[i])
+    for j in range(len(a)):
+        for k in range(j+1,len(a)):
+            if a[j]>a[k] and a[j] not in '`~!@#$%^&*()_-;:"<>,./?' and a[k] not in '`~!@#$%^&*()_-;:"<>,./?':
+                a[j],a[k]=a[k],a[j]
+    a = str(a)
+    a = a.replace("[","")
+    a = a.replace("]","")
+    a = a.replace(" ","")
+    a = a.replace(",","")
+    a = a.replace("[","")
+    a = a.replace("'","")
+    print(a,end=" ")

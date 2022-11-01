994a413778548a8f7d191cfddb256f1b205041fe
21A91A05D8 <97424615+21A91A05D8@users.noreply.github.com>
2022-11-01 18:07:18 +0530

    s=input().split() p=[] for i in s:     p.append(i[::-1]) print(" ".join(p))

--- reverse_every_word.py
+++ reverse_every_word.py
@@ -1,10 +1,5 @@
-def rev(s):
-    l=len(s)
-    s1=''
-    for j in range(l-1,-1,-1):
-        s1+=s[j]
-    return s1
-s=input()
-arr=list(s.split())
-for i in arr:
-    print(rev(i),end=" ")
+s=input().split()
+p=[]
+for i in s:
+    p.append(i[::-1])
+print(" ".join(p))

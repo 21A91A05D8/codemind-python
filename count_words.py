6b4d9de8db82f4457592428be24000ef360f9665
21A91A05D8 <97424615+21A91A05D8@users.noreply.github.com>
2022-11-01 11:43:03 +0530

    def cntwords(str):     vowel=set('aeiouAEIOU')     c=0     for i in str.split():         i=list(i)         n=len(i)-1         if i[0] in vowel and i[n] not in vowel:             c=c+1     return c str=input() print(cntwords(str))

--- count_words.py
+++ count_words.py
@@ -1,7 +1,11 @@
-s=input().split()
-c=0
-v='aeiouAEIOU'
-for i in s:
-    if i[0] in v and i[len(i)-1] not in v:
-        c+=1
-print(c)
+def cntwords(str):
+    vowel=set('aeiouAEIOU')
+    c=0
+    for i in str.split():
+        i=list(i)
+        n=len(i)-1
+        if i[0] in vowel and i[n] not in vowel:
+            c=c+1
+    return c
+str=input()
+print(cntwords(str))

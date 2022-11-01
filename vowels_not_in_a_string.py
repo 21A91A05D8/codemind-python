9e8d69df37ea43d5b76cd878863b7627f255bfac
21A91A05D8 <97424615+21A91A05D8@users.noreply.github.com>
2022-11-01 10:32:19 +0530

    s=input() v='aeiou' c=0 for i in v:     if i not in s:         print(i,end=" ")         c=1 if c==0:     print(c)

--- vowels_not_in_a_string.py
+++ vowels_not_in_a_string.py
@@ -1,10 +1,9 @@
-n=input()
-vo='aeiou'
+s=input()
+v='aeiou'
 c=0
-for i in vo:
-    if i not in n:
-        print(i,end=' ')
-        c+=1
+for i in v:
+    if i not in s:
+        print(i,end=" ")
+        c=1
 if c==0:
-    print(0)
-        
+    print(c)
